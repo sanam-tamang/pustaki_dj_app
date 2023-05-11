0a2a5289699f875008e508368e2bf860950ba127
sanam-tamang <snm.tmg7@gmail.com>
2023-05-11 21:50:33 +0545

    changing usermodel property ang on debug false

--- account/models.py
+++ account/models.py
@@ -57,7 +57,7 @@ class User(AbstractBaseUser):
   objects = UserManager()
 
   USERNAME_FIELD = 'email'
-  REQUIRED_FIELDS = []
+  REQUIRED_FIELDS = ['first_name','last_name']
 
   def __str__(self):
       return self.email
