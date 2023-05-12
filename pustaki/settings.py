24393307b27219447df6a88a854d904789044896
sanam-tamang <snm.tmg7@gmail.com>
2023-05-12 19:30:53 +0545

    media field added

--- pustaki/settings.py
+++ pustaki/settings.py
@@ -133,6 +133,15 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SESSION_COOKIE_SECURE = True
+
+
+MEDIA_URL = 'media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+
 
 
 REST_FRAMEWORK = {
