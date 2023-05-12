f84670d4d2e3c1f02cfb330ca84a82895d82d937
sanam-tamang <snm.tmg7@gmail.com>
2023-05-12 18:15:58 +0545

    added + static in url

--- pustaki/urls.py
+++ pustaki/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -24,8 +23,4 @@ urlpatterns = [
     path('', include('book.urls')),
     path('user/', include('account.urls')),
     path('api/', include('tokengen.urls')),
-]
-urlpatterns += staticfiles_urlpatterns()
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
