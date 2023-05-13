fcc15d02ae55936d0434c0d894bde079949ca2f2
sanam-tamang <snm.tmg7@gmail.com>
2023-05-13 17:24:42 +0545

    fixing migrations erros

--- account/views.py
+++ account/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .serializers import UserLoginSerializer, UserRegistrationSerializer, UserDetailSerializer
+from .serializers import  UserRegistrationSerializer, UserDetailSerializer
 from rest_framework import status
 from django.contrib.auth import authenticate, login, logout
 from tokengen.models import get_tokens_for_user
