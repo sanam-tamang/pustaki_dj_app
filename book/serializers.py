d7ea313f7ceee780c4e2afa90109a2cd46d49225
sanam-tamang <snm.tmg7@gmail.com>
2023-05-16 17:01:52 +0545

    rollback to previous version

--- book/serializers.py
+++ book/serializers.py
@@ -4,7 +4,7 @@ from account.serializers import UserDetailSerializer
 from .models import *
 import uuid
 class AuthorSerializer(serializers.ModelSerializer):
-    # id = serializers.UUIDField()
+    id = serializers.UUIDField()
     class Meta:
         model = Author
         fields = '__all__'
@@ -17,31 +17,23 @@ class CategorySerializer(serializers.ModelSerializer):
 
 
 class BookSerializer(serializers.ModelSerializer):
-    # authors = AuthorSerializer()
-    # category = CategorySerializer()
-    # published_by =  UserDetailSerializer(read_only=True, many=False)
+    authors = AuthorSerializer()
     class Meta:
         model = Book
         fields = '__all__'
     def create(self,validated_data):
-        # authors_data= validated_data.pop('authors')
-       
-       
+        authors_data= validated_data.pop('authors')
         
-       
-        
-        # try:
-        #     author = Author.objects.get(id=authors_data['id'])
-        # except Author.DoesNotExist:
-        #     author = Author(id=uuid.uuid1, name= authors_data['name'])
-        #     author.save()
-          
-        
-        # book = Book.objects.create(authors=author, **validated_data)
-        # return book
-    
-        book = Book.objects.create( **validated_data)
+        try:
+            author = Author.objects.get(id=authors_data['id'])
+        except Author.DoesNotExist:
+            author = Author(id=uuid.uuid1, name= authors_data['name'])
+            author.save()
+
+        book = Book.objects.create(authors=author, **validated_data)
         return book
+    
+        
         
 
 
