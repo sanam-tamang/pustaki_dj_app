aac8eee7455e767214d9c60208c7d216e7f52402
sanam-tamang <snm.tmg7@gmail.com>
2023-05-18 05:40:39 +0545

    added get method for getting list of category

--- book/views.py
+++ book/views.py
@@ -38,3 +38,12 @@ class  AddtCategoryView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class  GetCategoryListView(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self,request):
+        categoryList = Category.objects.all()
+        serializers = CategorySerializer(categoryList, many=True)
+        return Response (serializers.data, status=status.HTTP_200_OK)
+   
