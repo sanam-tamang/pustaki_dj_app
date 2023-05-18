4ebb78ffdccbd8a546d29b8a971c6db7975e4d87
sanam-tamang <snm.tmg7@gmail.com>
2023-05-18 17:25:02 +0545

    change in contentfilename

--- book/models.py
+++ book/models.py
@@ -5,7 +5,7 @@ import os
 
 def content_file_name(instance, filename):
     ext = filename.split('.')[-1]
-    filename = "%s_%s.%s" % (instance.published_by.id, uuid.uuid4(), ext)
+    filename = "%s.%s" % ( uuid.uuid4(), ext)
     return os.path.join('uploads', filename)
 
 class Category(models.Model):
