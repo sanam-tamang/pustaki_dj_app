b2af1810af9a8da6afafa3bdceb0cad8e1e7397f
sanam-tamang <snm.tmg7@gmail.com>
2023-06-07 16:53:58 +0545

    allowing header * in cors

--- pustaki/settings.py
+++ pustaki/settings.py
@@ -151,7 +151,7 @@ REST_FRAMEWORK = {
 
 # CORS_ORIGIN_ALLOW_ALL = True
 # CORS_ALLOW_CREDENTIALS = True
-# CORS_ALLOW_HEADERS = ['*']
+CORS_ALLOW_HEADERS = ['*']
 CORS_ORIGIN_WHITELIST = ('https://pustakidjapp-production.up.railway.app',)
 
 
